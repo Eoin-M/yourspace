10c4eb1377337508a024a8e42e823b9f9ceed07c
David Byrne <David.dbyrne@gmail.com>
2015-09-23 15:09:21 +0100

    location functions now have their own specific js file
    Just for testing and will be updated in the future which will break
    anything based on this version.

--- location.js
+++ location.js
@@ -1,20 +1,25 @@
-//IMPORTANT: Needs <script type="text/javascript" src="http://maps.googleapis.com/maps/api/js?sensor=false"></script> to run
+/* Up to date version of location functions which replaces david.js file. They will be changed in future and I'm planning on adding a location object 
+	as a global varibale instead but this works for now. To get location in longtitude and latitude call the myGetLocation()
+	function and the global variables lat and long will be changed. 
+*/
 var lat;
 var long;
-//window.onload = myGetLocation()
-	
+
 function myGetLocation()
 {
-	if (navigator.geolocation)
+	if (navigator.geolocation)//if navigator geolocation is supported then
 	{
-		navigator.geolocation.getCurrentPosition( positionToLongLat , noLocationError )
+		navigator.geolocation.getCurrentPosition( positionToLongLat , noLocationError );
+		//In form of .getCurrentPosition(successFunction, failLocation)
 	}
 	else
 	{
 		console.log("Geolocation is not supported by this browser.");
+		//return false;
 	}
+	//TODO wait for set time, then return false if 
 }
-	
+
 function positionToLongLat(position)
 {
 	lat = position.coords.latitude;
@@ -41,7 +46,7 @@ function noLocationError(error) //should work but I've never seen it called...
 			break;
 	}
 }
-		
+
 function runGoogleNavigatorAPI()
 //http://stackoverflow.com/questions/6797569/get-city-name-using-geolocation source
 {
@@ -51,10 +56,9 @@ function runGoogleNavigatorAPI()
 	{//start of anonymous function taken as the second input of geocoder.geocode method
 		if (status == google.maps.GeocoderStatus.OK)
 		{
-			console.log(results);
 			if (results[0])//says (results[1]) in original source?
 			{
-				alert(results[0].formatted_address);
+				console.log(results[0].formatted_address);
 				//TODO: results object can be further parsed to get city, country etc.
 			}
 			else
