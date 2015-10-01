ad1a34a5bc783b489510ebaadc9b113dd1734532
David Byrne <David.dbyrne@gmail.com>
2015-10-01 22:05:24 +0100

    Added basic stocks functionality to index.html

---
+++ stocks.js
@@ -0,0 +1,43 @@
+function getStockData(symbol)
+{
+	var url = "http://query.yahooapis.com/v1/public/yql";
+	//var symbol = $("#symbol").val();
+    var data = encodeURIComponent("select * from yahoo.finance.quotes where symbol in ('" + symbol + "')");
+	
+	$.getJSON(url, 'q=' + data + "&format=json&diagnostics=true&env=http://datatables.org/alltables.env").done(function (data) 
+	{
+		console.log(data.query.results.quote);
+		$("#stockName").text("Name: " + data.query.results.quote.Symbol);
+		//$("#date").text("Bid Price: " + data.query.results.quote.Date);
+		//$("#time").text("Bid Price: " + data.query.results.quote.LastTradeTime);
+		$("#stockResult").text("Price: " + data.query.results.quote.LastTradePriceOnly);
+		$("#stockChange").text("Change: " + data.query.results.quote.PercentChange);
+		//$("#bid").text("Bid Price: " + data.query.results.quote.LastTradePriceOnly);
+		//$("#ask").text("Bid Price: " + data.query.results.quote.Ask);
+		//("#volume").text("Bid Price: " + data.query.results.quote.Volume);
+		//$("#high").text("Bid Price: " + data.query.results.quote.HighLimit);
+		//$("#low").text("Bid Price: " + data.query.results.quote.LowLimit);
+
+		var change = parseInt(data.query.results.quote.PercentChange,10);//10 here means base 10
+		if(change >= 0)
+		{
+			document.getElementById("stockChange").className = "greenText";
+		}
+		else
+		{
+			document.getElementById("stockChange").className = "redText";
+		}
+
+	}).fail(function (jqxhr, textStatus, error) 
+	{
+		var err = textStatus + ", " + error;
+		console.log("Error with getting stocks: "+ textStatus+" , "+err);
+	});
+}
+
+//function checkEnter(e) --Might need to be brought back, CORS doesn't like if we call the api from a form...
+
+function searchStock()
+{
+	getStockData(document.getElementById("queryStockName").value);
+}
