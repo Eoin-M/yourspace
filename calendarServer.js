1a4e31657deeee370ef0b405868a64c2cbb8247f
David Byrne <David.dbyrne@gmail.com>
2015-10-30 22:08:34 +0000

    Added basic multi-calendar support
    Also removed some of the excessive default commenting and improved the
    indentation.

--- calendarServer.js
+++ calendarServer.js
@@ -29,168 +29,184 @@ var CAL_RES;//calendar response global variable
 var usersCode = null;
 
 
-
-/**
- * Create an OAuth2 client with the given credentials, and then execute the
- * given callback function.
- *
- * @param {Object} credentials The authorization client credentials.
- * //@param {function} callback The callback to call with the authorized client.
- */
-
-
-
-
-
 function authorize(callback) {
   
+	//var clientSecret = "Check dropbox";
+	//var clientId = "check dropbox";
+	  
+	var redirectUrl = "http://localhost:8080";
+	var auth = new googleAuth();
+	var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
 
-  //var clientSecret = "Check dropbox";
-  //var clientId = "check dropbox";
-  
-  var redirectUrl = "http://localhost:8080";
-  var auth = new googleAuth();
-  var oauth2Client = new auth.OAuth2(clientId, clientSecret, redirectUrl);
-
-  // Check if we have previously stored a token.
-  //console.log(CAL_TOKEN_PATH +"=============================================");
-  fs.readFile(CAL_TOKEN_PATH, function(err, token) {
-    if (err) {
-      console.log("getting new token ------------------------------------------");
-      getNewToken(oauth2Client, callback);
-    } else {
-      console.log("getting oldtoken ------------------------------------------");
-      oauth2Client.credentials = JSON.parse(token);
-      console.log(token +"--------------------------------------------------------");
-      callback(oauth2Client);
-    }
-    /*console.log("getting new token ------------------------------------------");
-    getNewToken(oauth2Client, callback);*/
-  });
+	// Check if we have previously stored a token.
+	//console.log(CAL_TOKEN_PATH +"=============================================");
+	fs.readFile(CAL_TOKEN_PATH, function(err, token) 
+	{
+		if (err) 
+		{
+			console.log("getting new token ------------------------------------------");
+			getNewToken(oauth2Client, callback);
+		} 
+		else 
+		{
+			console.log("getting oldtoken ------------------------------------------");
+			oauth2Client.credentials = JSON.parse(token);
+			console.log(token +"--------------------------------------------------------");
+			callback(oauth2Client);
+		}
+	});
 }
 
-/**
- * Get and store new token after prompting for user authorization, and then
- * execute the given callback with the authorized OAuth2 client.
- *
- * @param {google.auth.OAuth2} oauth2Client The OAuth2 client to get token for.
- * @param {getEventsCallback} callback The callback to call with the authorized
- *     client.
- */
+
 function getNewToken(oauth2Client, callback) {
-  var SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
-  var authUrl = oauth2Client.generateAuthUrl({
-    access_type: 'offline',
-    scope: SCOPES
-  });
-  if (usersCode == null)
-  {
-    var custEvents = [];
-    var tempObj = {}
-    tempObj.auth = false;
-    tempObj.link = authUrl;
-    custEvents[0] = tempObj;
-    CAL_RES.setHeader('Content-Type', 'application/json');
-	  CAL_RES.send(custEvents);
-  }
-  else{
-    oauth2Client.getToken(usersCode, function(err, token) {
-        if (err) {
-          console.log("code" +usersCode);
-          console.log('Error while trying to retrieve access token', err);
-          return;
-        }
-        oauth2Client.credentials = token;
-        storeToken(token);
-        callback(oauth2Client);
-      });
-  }
+	var SCOPES = ['https://www.googleapis.com/auth/calendar.readonly'];
+	var authUrl = oauth2Client.generateAuthUrl(
+	{
+		access_type: 'offline',
+		scope: SCOPES
+	});
+	if (usersCode == null)
+	{
+		var custEvents = [];
+		var tempObj = {}
+		tempObj.auth = false;
+		tempObj.link = authUrl;
+		custEvents[0] = tempObj;
+		CAL_RES.setHeader('Content-Type', 'application/json');
+		CAL_RES.send(custEvents);
+	}
+	else
+	{
+		oauth2Client.getToken(usersCode, function(err, token) 
+		{
+			if (err) 
+			{
+				console.log("code" +usersCode);
+				console.log('Error while trying to retrieve access token', err);
+				return;
+			}
+			oauth2Client.credentials = token;
+			storeToken(token);
+			callback(oauth2Client);
+		});
+	}
 }
 
-/**
- * Store token to disk be used in later program executions.
- *
- * @param {Object} token The token to store to disk.
- */
+
 function storeToken(token) {
-  try {
-    fs.mkdirSync(CAL_TOKEN_DIR);
-  } catch (err) {
-    if (err.code != 'EEXIST') {
-      throw err;
-    }
-  }
-  fs.writeFile(CAL_TOKEN_PATH, JSON.stringify(token));
-  console.log('Token stored to ' + CAL_TOKEN_PATH);
+	try 
+	{
+		fs.mkdirSync(CAL_TOKEN_DIR);
+	} 
+	catch (err) 
+	{
+		if (err.code != 'EEXIST') 
+		{
+			throw err;
+		}
+	}
+	fs.writeFile(CAL_TOKEN_PATH, JSON.stringify(token));
+	console.log('Token stored to ' + CAL_TOKEN_PATH);
 }
 
-/**
- * Lists the next 10 events on the user's primary calendar.
- *
- * @param {google.auth.OAuth2} auth An authorized OAuth2 client.
- */
-function listEvents(auth) {
-  var calendar = google.calendar('v3');
-  calendar.events.list({
-    auth: auth,
-    //calendarId: 'ta2m8hkjtdp5pva728eo3ep6v4@group.calendar.google.com',//'primary'
-    calendarId: 'primary',
-    timeMin: (new Date()).toISOString(),//today is the min date
-    maxResults: 7,//number of events returned
-    singleEvents: true,
-    orderBy: 'startTime'
-  }, function(err, response) {
-    if (err) {
-      console.log('The API returned an error: ' + err);
-      return;
-    }
-    
-    var events = response.items;
-    var custEvents = [];
-    var tempObj = {};
-    tempObj.auth = true;
-    tempObj.link = "";
-    custEvents[0] = tempObj;
-    if (events.length == 0) {
-      console.log('No upcoming events found.');
-    } else {
-      console.log('Upcoming 10 events:');
-      for (var i = 0; i < events.length; i++) {
+
+function listEvents(auth) 
+{
+   
+   var calendar = google.calendar('v3');
+   var custEvents = [];//the array we'll eventually be returning to the browser
+   calendar.calendarList.list(
+   {
+    auth: auth
+   }, function(err, response) {
+        if (err) 
+		{
+			console.log('126: Calendar not found... The API returned an error: ' + err);
+			return;
+        }
+        var tempObj = {};
+        tempObj.auth = true;
+        tempObj.link = "";
+        custEvents[0] = tempObj;
         
-        var event = events[i];
-        var eventObj = {};
-        eventObj.start = new Date(event.start.dateTime || event.start.date);
-        eventObj.end = new Date(event.end.dateTime || event.end.date);
-        var timeDiff = Math.abs(eventObj.end.getTime() - eventObj.start.getTime());
-        eventObj.diff = Math.ceil(timeDiff / (1000 * 60));
-        eventObj.description = event.summary;
-        if (event.colorId == null)
+        var calendars = response.items;
+        if (calendars.length == 0) 
         {
-          event.colorId='-1';
-        }
-        if (event.location == null)
+			console.log('No calendars found.');
+        } 
+        else 
         {
-          event.location=false;
+			//console.log(calendars);
+			var i = 0;
+			for (i = 0; i < calendars.length; i++)
+			{
+				//console.log(calendars[i].id);
+				calendar.events.list(
+				{
+				  auth: auth,
+				  calendarId: calendars[i].id,
+				  timeMin: (new Date()).toISOString(),//today is the min date
+				  //TODO add in min and max dates so we get one day's events
+				  maxResults: 5,//number of events returned
+				  singleEvents: true,
+				  orderBy: 'startTime'
+				}, 
+				function(err, response) 
+				{
+					if (err) 
+					{
+						console.log('ln:159 The API returned an error: ' + err);
+						return;
+					}
+				  
+					var events = response.items;
+					if (events != null)
+					{
+						for (var j = 0; j < events.length; j++) 
+						{
+							var eventObj = {};
+							var event = events[j];
+							//console.log("EVENTS===" +events[j]);
+							eventObj.start = new Date(event.start.dateTime || event.start.date);
+							eventObj.end = new Date(event.end.dateTime || event.end.date);
+							var timeDiff = Math.abs(eventObj.end.getTime() - eventObj.start.getTime());
+							eventObj.diff = Math.ceil(timeDiff / (1000 * 60));
+							eventObj.description = event.summary;
+							if (event.colorId == null)
+							{
+								event.colorId='-1';
+							}
+							if (event.location == null)
+							{
+								event.location=false;
+							}
+							eventObj.colorID = event.colorId;
+							eventObj.location = event.location;
+							eventObj.link = event.htmlLink;
+							custEvents[custEvents.length] = eventObj;
+							//console.log(eventObj);
+						}
+					}
+				}
+            )}
         }
-        eventObj.colorID = event.colorId;
-        eventObj.location = event.location;
-        eventObj.link = event.htmlLink;
-        console.log(util.format('========================================').cyan);
-        console.log(event);
-        //console.log(eventObj);
-        //console.log('%s - %s', start, event.summary);
-        custEvents[i+1] = eventObj;
-      }
-    }
-    CAL_RES.setHeader('Content-Type', 'application/json');
-	  CAL_RES.send(custEvents);
-  });
-  
+		setTimeout(returnCalData, 500, custEvents);//Temp solution until a way is found to know when all events are returned TODO
+	});
 }
+  
+  
+function returnCalData(eventsAry)
+{
+	//console.log(util.format("CUSTEVENTS ===="+eventsAry).cyan);
+	CAL_RES.setHeader('Content-Type', 'application/json');
+	CAL_RES.send(eventsAry);
+} 
+      
+
 
 app.get('/', function (req,res)
 {
-  usersCode = req.query.code;//reference: http://javascriptplayground.com/blog/2013/06/node-and-google-oauth/
+	usersCode = req.query.code;//reference: http://javascriptplayground.com/blog/2013/06/node-and-google-oauth/
 	console.log("user connnected");
 	res.sendfile("calendar.html");
 });
@@ -198,22 +214,17 @@ app.get('/', function (req,res)
 app.post('/nextEvents', function (req, res)
 {
 	console.log("hit");
-  CAL_REQ = req;
-  CAL_RES = res;
-  
-  //STEP 1:first gets authorisation to access calendar
-  fs.readFile('client_secret.json', function processClientSecrets(err, content) {
-  if (err) 
-  {
-    console.log('Error loading client secret file: ' + err);
-  }
-    // Authorize a client with the loaded credentials, then call the
-    // Google Calendar API.
-    authorize(listEvents);
-    console.log(content);
-  });
-	//END STEP 1
-  
+	CAL_REQ = req;
+	CAL_RES = res;
+	
+	fs.readFile('client_secret.json', function processClientSecrets(err, content) 
+	{
+		if (err) 
+		{
+			console.log('Error loading client secret file: ' + err);
+		}
+		authorize(listEvents);
+	});
 });
 
 app.listen(8080);
